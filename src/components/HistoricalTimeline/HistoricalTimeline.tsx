872818196409155d893e8b997049a60573bab811
Amirah Zulaikha <45570327+amirahazhari@users.noreply.github.com>
2024-08-28 17:49:46 +0800

    Initial commit

---
+++ src/components/HistoricalTimeline/HistoricalTimeline.tsx
@@ -0,0 +1,211 @@
+import { memo } from "react";
+import type { FC } from "react";
+
+import resets from "../_resets.module.css";
+import { Capa_1Icon } from "./Capa_1Icon";
+import { Frame427319160Icon } from "./Frame427319160Icon";
+import { Frame427319160Icon2 } from "./Frame427319160Icon2";
+import { FrameIcon } from "./FrameIcon";
+import classes from "./HistoricalTimeline.module.css";
+
+interface Props {
+  className?: string;
+}
+/* @figmaId 8349:135019 */
+export const HistoricalTimeline: FC<Props> = memo(function HistoricalTimeline(
+  props = {}
+) {
+  return (
+    <div className={`${resets.clapyResets} ${classes.root}`}>
+      <div className={classes.frame427319192}>
+        <div className={classes.frame427319188}>
+          <div className={classes.frame427319161}>
+            <div className={classes.frame48096735}>
+              <div className={classes._1910}>1910</div>
+              <div className={classes.frame48096730}>
+                <div className={classes.discovery}>Discovery</div>
+              </div>
+            </div>
+            <div className={classes.frame427319160}>
+              <Frame427319160Icon className={classes.icon} />
+            </div>
+          </div>
+          <div className={classes.frame427319183}>
+            <div className={classes.frame427319179}>
+              <div className={classes.frame427319175}>
+                <div className={classes.discoveryOfMalaysiaSFirstOilWe}>
+                  Discovery of Malaysia&#39;s First Oil Well
+                </div>
+              </div>
+            </div>
+            <div className={classes.frame427319174}>
+              <div className={classes.frame427319171}>
+                <div className={classes.frame427319168}>
+                  <div className={classes._19102}>1910</div>
+                </div>
+                <div className={classes.shellDiscoversMalaysiaSFirstOi}>
+                  Shell discovers Malaysia&#39;s first oil well, Miri No. 1, on
+                  Canada Hill in Miri, Sarawak.
+                </div>
+              </div>
+              <div className={classes.frame427319172}>
+                <div className={classes.frame427319169}>
+                  <div className={classes._1910December}>1910 (December)</div>
+                </div>
+                <div className={classes.miriNo1BeginsProductionYieldin}>
+                  Miri No. 1 begins production, yielding 83 barrels per day.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={classes.frame427319189}>
+          <div className={classes.frame427319162}>
+            <div className={classes.frame48096736}>
+              <div className={classes._19291945}>1929 - 1945</div>
+              <div className={classes.frame480967302}>
+                <div className={classes.peakProduction}>Peak Production</div>
+              </div>
+            </div>
+            <div className={classes.frame4273191602}>
+              <div className={classes.capa_1}>
+                <Capa_1Icon className={classes.icon2} />
+              </div>
+            </div>
+          </div>
+          <div className={classes.frame427319184}>
+            <div className={classes.frame427319180}>
+              <div className={classes.frame427319176}>
+                <div className={classes.peakProductionAtMiriField}>
+                  Peak Production at Miri Field
+                </div>
+              </div>
+            </div>
+            <div className={classes.frame427319181}>
+              <div className={classes.frame4273191712}>
+                <div className={classes.frame4273191682}>
+                  <div className={classes._1929}>1929</div>
+                </div>
+                <div className={classes.productionAtMiriFieldPeaksAt15}>
+                  Production at Miri field peaks at 15,000 barrels per day.
+                </div>
+              </div>
+              <div className={classes.frame4273191722}>
+                <div className={classes.frame4273191692}>
+                  <div className={classes.wW2Era}>WW2 Era</div>
+                </div>
+                <div className={classes.miriFieldExperiencesDamageAndD}>
+                  Miri field experiences damage and decline due to scorched
+                  earth policies and bombings.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={classes.frame427319190}>
+          <div className={classes.frame427319163}>
+            <div className={classes.frame48096737}>
+              <div className={classes._1950s1960s}>1950s - 1960s</div>
+              <div className={classes.frame480967303}>
+                <div className={classes.expansion}>Expansion</div>
+              </div>
+            </div>
+            <div className={classes.frame4273191603}>
+              <Frame427319160Icon2 className={classes.icon3} />
+            </div>
+          </div>
+          <div className={classes.frame427319186}>
+            <div className={classes.frame4273191812}>
+              <div className={classes.frame427319177}>
+                <div className={classes.expansionOfActivitiesInBorneo}>
+                  Expansion of Activities in Borneo
+                </div>
+              </div>
+            </div>
+            <div className={classes.frame427319185}>
+              <div className={classes.frame4273191713}>
+                <div className={classes.frame4273191683}>
+                  <div className={classes._1950s}>1950s</div>
+                </div>
+                <div className={classes.noDrillingActivitiesOccurElsew}>
+                  No drilling activities occur elsewhere in Borneo or Malaya.
+                </div>
+              </div>
+              <div className={classes.frame4273191723}>
+                <div className={classes.frame4273191693}>
+                  <div className={classes._1960s}>1960s</div>
+                </div>
+                <div className={classes.petroleumActivitiesExpandInBor}>
+                  Petroleum activities expand in Borneo with the discovery and
+                  development of offshore fields. Shell, followed by Esso,
+                  dominates upstream production, downstream refining, and sales.
+                </div>
+              </div>
+              <div className={classes.frame427319173}>
+                <div className={classes.frame427319170}>
+                  <div className={classes.late1960s}>Late 1960s</div>
+                </div>
+                <div className={classes.foreignPetroleumCompaniesExplo}>
+                  Foreign petroleum companies explore offshore Peninsular
+                  Malaysia and Esso and Conoco receive concessions for oil and
+                  gas exploration off the east coast of the Peninsular.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={classes.frame427319191}>
+          <div className={classes.frame427319164}>
+            <div className={classes.frame48096738}>
+              <div className={classes._1974Current}>1974 - Current</div>
+              <div className={classes.frame480967304}>
+                <div className={classes.establishmentAuthority}>
+                  Establishment &amp; Authority
+                </div>
+              </div>
+            </div>
+            <div className={classes.frame4273191604}>
+              <div className={classes.frame}>
+                <FrameIcon className={classes.icon4} />
+              </div>
+            </div>
+          </div>
+          <div className={classes.frame427319187}>
+            <div className={classes.frame427319182}>
+              <div className={classes.frame427319178}>
+                <div className={classes.establishmentAuthorityOfPETRON}>
+                  Establishment &amp; Authority of PETRONAS
+                </div>
+              </div>
+            </div>
+            <div className={classes.frame4273191852}>
+              <div className={classes.frame4273191714}>
+                <div className={classes.frame4273191684}>
+                  <div className={classes._1974}>1974</div>
+                </div>
+                <div className={classes.dueToTheVariousInfluentialEcon}>
+                  <div className={classes.textBlock}>
+                    Due to the various influential economic and political
+                    factors, alongside the rise of economic nationalism in
+                    Malaysia leading to the implementation of the New Economic
+                    Policy (NEP), PETRONAS was established in 1974 under the
+                    Petroleum Development Act 1974 (PDA 1974).
+                  </div>
+                  <div className={classes.textBlock2}>
+                    <p></p>
+                  </div>
+                  <div className={classes.textBlock3}>
+                    In accordance with the Petroleum Development Act 1974 (PDA
+                    1974), Malaysia maintains complete authority and control
+                    over all petroleum resources within its territory and
+                    possesses the ability to enforce its jurisdiction.
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+});
